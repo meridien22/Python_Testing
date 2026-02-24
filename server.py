139db594ca34513f432e3396dc7bd4953cb154cc
meridien22 <meridien.sig@gmail.com>
2026-02-24 15:20:03 +0100

    FIX:BR4-BUG-Clubs-book-more-than-12-places

--- server.py
+++ server.py
@@ -46,9 +46,14 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+
+    if placesRequired > 12:
+        flash("You can't book more than 12 places.")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else :
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        flash('Great, booking complete!')
+        return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
